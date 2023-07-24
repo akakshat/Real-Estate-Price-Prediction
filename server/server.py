545307ad4ef9eaf5f6124dd2ca5997c848482141
Akshat Garg <akasatgarg@gmail.com>
2023-07-24 18:08:21 +0530

    Build the python flask server

--- server/server.py
+++ server/server.py
@@ -11,9 +11,21 @@ def get_location_names():
     # The response also has the Access-Control-Allow-Origin header set to *,
     # which allows the response to be accessed from any origin.
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
--- server/util.py
+++ server/util.py
@@ -1,9 +1,28 @@
 import json
 import pickle
+import numpy as np
 
 __locations = None
 __data_columns = None
 __model = None
+
+def get_estimated_price(location, sqft, bhk, bath):
+
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+    #  the value is in float that's why we use round
+    #  we need this float no. in two decimal places that why we use 2
+    return round(__model.predict([x])[0], 2)
+
 
 def get_location_names():
     return __locations
@@ -19,6 +38,7 @@ def load_saved_artifacts():
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
+    global __model
     #  since it is a binary model it is rb
     with open("./artifacts/banglore_house_data_model.pickle", 'rb') as f:
         __model = pickle.load(f)
@@ -27,3 +47,7 @@ def load_saved_artifacts():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    print(get_estimated_price('Ejipura', 1000, 3, 2))  # other location
